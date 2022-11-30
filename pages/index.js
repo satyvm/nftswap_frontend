6073b81a9d7ad0ef286e1610f326431f9ccc75d4
Satyam <na20b060@smail.iitm.ac.in>
2022-11-30 16:34:53 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import NavBar from "./components/navbar"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div >
+      <NavBar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
