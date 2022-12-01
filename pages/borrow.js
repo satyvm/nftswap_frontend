4c4e87e1b2a8547e008e0c9d3aa0b69bbeef711e
ssd39 <saurabh.sachit39@gmail.com>
2022-12-01 08:59:41 +0530

    web3 integration

--- pages/borrow.js
+++ pages/borrow.js
@@ -3,7 +3,8 @@ import NavBar from './components/navbar'
 import ConnectMeta from './components/connectMeta'
 import NftCard from './components/nftCard'
 import { TailSpin } from 'react-loader-spinner';
-
+import Web3 from 'web3';
+import gateway from '../gateway';
 function ConnectDiv({ isLoading, ...props }) {
     return (
       <div {...props}>
@@ -21,23 +22,25 @@ function ConnectDiv({ isLoading, ...props }) {
 
 export default function Borrow(){
     const [nftList,setNftList] = useState([])
+    const [currentAccount, setCurrentAccount] = useState('');
+
     useEffect(()=>{
-        const address = "0xf36F155486299eCAff2D4F5160ed5114C1f66000";
-        fetch(`https://deep-index.moralis.io/api/v2/${address}/nft?chain=goerli&&format=decimal&&normalizeMetadata=false`,{
-            headers: {
-                accept: 'application/json', 
-                'X-API-Key': 'RZgl1p2CkA8Nc0U5Nao8GfGTqlmDT3U7GbExOaj0cAAjtIru1fGXyS7jmCdlxnDQ'
-            }
-        }).then((res)=>res.json())
-        .then((res)=>setNftList(res.result))
-        .catch((e)=>{
-            console.error(e)
-        })
-    },[])
+        if(currentAccount){
+            fetch(`https://deep-index.moralis.io/api/v2/${currentAccount}/nft?chain=goerli&&format=decimal&&normalizeMetadata=false`,{
+                headers: {
+                    accept: 'application/json', 
+                    'X-API-Key': 'RZgl1p2CkA8Nc0U5Nao8GfGTqlmDT3U7GbExOaj0cAAjtIru1fGXyS7jmCdlxnDQ'
+                }
+            }).then((res)=>res.json())
+            .then((res)=>setNftList(res.result))
+            .catch((e)=>{
+                console.error(e)
+            })
+        }
+    },[currentAccount])
     
     const [isButtonLoading, setIsButtonLoading] = useState(false);
 
-    const [currentAccount, setCurrentAccount] = useState('');
     
     // Implement your connectWallet method here
     const connectWallet = async () => {
@@ -55,43 +58,22 @@ export default function Borrow(){
         // Boom! This should print out public address once we authorize Metamask.
         console.log("Connected", accounts[0]);
         setCurrentAccount(accounts[0]);
+        window.web3 = new Web3(ethereum);
+        window.contract = new web3.eth.Contract(gateway.abi, "0xdbCf54d2eB02bb2aFB9B728C44c8ec1B0780FF16"); 
         } catch (error) {
         console.log(error)
         }
     }
 
-    const checkIfWalletIsConnected = async () => {
-        const { ethereum } = window;
-
-        if (!ethereum) {
-        console.log('Make sure you have metamask!');
-        return;
-        } else {
-        console.log('We have the ethereum object', ethereum);
-        }
-
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-        if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log('Found an authorized account:', account);
-        setCurrentAccount(account);
-        } else {
-        console.log('No authorized account found');
-        }
-    };
-
     // Render Methods
     const renderNotConnectedContainer = () => (
         <div>
-        <ConnectDiv onClick={event => {setIsButtonLoading(true);
-        connectWallet();}} isLoading={isButtonLoading}></ConnectDiv>
+        <ConnectDiv onClick={event => {
+            setIsButtonLoading(true);
+            connectWallet();
+        }} isLoading={isButtonLoading}></ConnectDiv>
         </div>
     );
-    
-    useEffect(() => {
-        checkIfWalletIsConnected();
-    }, []);
 
     const renderConnectedContainer = () =>(
         <div>
@@ -119,7 +101,7 @@ export default function Borrow(){
                 <RenderDiv/>
             </div>
             
-            <div className="grid grid-cols-3 gap-4 m-10">
+            <div className="grid grid-cols-5 gap-4 m-10">
                 {
                     nftList.map((nft, i)=>{
                         return (
