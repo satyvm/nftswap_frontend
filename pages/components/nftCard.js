b72d70f247ba31ed4d90781bf3ad9a39c96ab45c
Satyam <na20b060@smail.iitm.ac.in>
2022-12-01 07:19:26 +0530

    Modal added

--- pages/components/nftCard.js
+++ pages/components/nftCard.js
@@ -1,20 +1,96 @@
-import { useEffect } from "react"
+import { useEffect, Fragment, useState  } from "react"
+import { Dialog, Transition } from '@headlessui/react'
+import SelectNetwork from "./selectNetwork"
 
 export default function NftCard({ metadata, symbol, token_address, token_id }){
-    useEffect(()=>{
-        console.log('here')
-    },[])
+    let [isOpen, setIsOpen] = useState(false)
+
+    function closeModal() {
+      setIsOpen(false)
+    }
+  
+    function openModal() {
+      setIsOpen(true)
+    }
+  
+
     return (
         <div className="glass_card font p-5"> 
             <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg" 
             className="bg-clip-text rounded-lg" ></img>
             <h1 className="text-2xl py-4 pb-1 px-2">{symbol}</h1>
-            <p className="p-1 px-2 pb-0 text-black/70 break-all">NFT Address: {token_address}</p>
-            <p className="p-1 px-2 text-black/70">TokendID: {token_id}</p>
+            <p className="p-1 px-2 pb-0 text-black/70 break-all"><span className="font-bold">NFT Address: </span>{token_address}</p>
+            <p className="p-1 px-2 text-black/70"><span className="font-bold">Tokend ID: </span>{token_id}</p>
             <div className="flex justify-center">
-            <button className="text-white rounded-lg bg-black/90 hover:bg-black/80 active:bg-white/10 focus:none p-3 px-5 m-3">
+            <button
+                type="button"
+                onClick={openModal}
+                className="text-white rounded-lg bg-black/90 hover:bg-black/80 active:bg-white/10 focus:none p-3 px-5 m-3"
+                >
                 Collateralize it
             </button>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                    >
+                        <Dialog.Panel className="w-full glass_card max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                        <div className="">
+                        <Dialog.Title
+                            as="h3"
+                            className="text-3xl px-4 pt-6"
+                        >
+                            Collateralize {symbol} now.
+                        </Dialog.Title>
+                        <div className="mt-2 text-sm p-4">
+                            <p className="text-black/70 break-all"><span className="font-bold">NFT Address: </span>{token_address}</p>
+                            <p className="text-black/70"><span className="font-bold">Tokend ID: </span>{token_id}</p>
+                        </div>
+                        </div>
+
+                        <div className="flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-20">
+                            <path fill-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v16.19l2.47-2.47a.75.75 0 111.06 1.06l-3.75 3.75a.75.75 0 01-1.06 0l-3.75-3.75a.75.75 0 111.06-1.06l2.47 2.47V3a.75.75 0 01.75-.75z" clip-rule="evenodd" />
+                        </svg>
+                        </div>
+                        <div className="p-4 pb-10">
+                            <SelectNetwork />
+                        </div>
+
+                        <div className="mt-2 flex justify-center">
+                            <button
+                            type="button"
+                            className="text-white rounded-lg bg-black/90 hover:bg-black/80 active:bg-white/10 focus:none p-3 px-5 m-3"
+                            onClick={closeModal}>
+                            Confirm
+                            </button>
+                        </div>
+                        </Dialog.Panel>
+                    </Transition.Child>
+                    </div>
+                </div>
+                </Dialog>
+            </Transition>
             </div>
         </div>
     )
---
+++ pages/components/selectNetwork.js
@@ -0,0 +1,68 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
+
+const networks = [
+  { name: 'Starknet' },
+  { name: 'Ethereum' },
+  { name: 'Others' },
+]
+
+export default function SelectNetwork() {
+  const [selected, setSelected] = useState(networks[0])
+
+  return (
+    <div>
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative glass w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute glass mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {networks.map((network, networkIdx) => (
+                <Listbox.Option
+                  key={networkIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-gray-100 text-black' : 'text-black'
+                    }`
+                  }
+                  value={network}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {network.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-black">
+                          <CheckIcon className="h-5 w-5 focus:text-white" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  )
+}
